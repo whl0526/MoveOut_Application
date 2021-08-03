3a18b62580aeca74090aa8ce23808fbf9fa5a6e5
sjms321 <sjms321@naver.com>
2021-08-03 23:25:26 +0900

    make calendar fiction(just can pop up calendar) to reservation view

---
+++ move_application/lib/view/aa.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class a extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+
+}
--- move_application/lib/view/reservation.dart
+++ move_application/lib/view/reservation.dart
@@ -1,23 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
+class Reservation extends StatefulWidget{
+  @override
+  _Reservation createState() => _Reservation();
 
-class Reservation extends StatelessWidget {
+}
+
+class _Reservation extends State<Reservation> {
+
+  String _value = '아직 날짜 선택 안됐음';
+
+  Future _selectDate() async {
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime.now(),
+        lastDate: DateTime(2030),
+    );
+    if(picked != null) setState(() => _value = picked.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body:
       Container(
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-              child: Text(
-                "예약",
-                style: TextStyle(fontSize: 20),
-              ))
-        ],
-      ),
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            IconButton(icon: Icon(Icons.favorite_border), onPressed: () async {
+              _selectDate();
+              print(_value);
+            },),
+            Container(
+              child: Text(_value),
+
+            ),
+            IconButton(icon: Icon(Icons.favorite_border), onPressed: () async {
+              _selectDate();
+              print(_value);
+            },),
+            Container(
+              child: Text(_value),
+
+            ),
+          ],
+        ),
       ),
     );
   }
+
+
 }
