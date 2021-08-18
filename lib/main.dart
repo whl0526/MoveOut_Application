9032b337f756cecf8c630dedfe6dbc7b04aaeb81
sjms321 <sjms321@naver.com>
2021-08-18 22:22:18 +0900

    fix title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Move App',
+      title: 'ABC App',
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
