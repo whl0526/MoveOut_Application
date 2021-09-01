a9fd0f3b53f25c19edc7c074181827040f62075a
whl0526 <whl0526@naver.com>
2021-09-01 22:30:41 +0900

    add icons_jpg change reservation.dart design

--- lib/view/reservation.dart
+++ lib/view/reservation.dart
@@ -17,37 +17,32 @@ class _Reservation extends State<Reservation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Column(
-
-         // crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-
-            SizedBox(
-              height: 50,
-            ),
-            Container(
-
-              height:175,
-              margin: EdgeInsets.all(15),
-
-              child:ListView(
-                scrollDirection: Axis.horizontal,
+      body: Container(
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        onTap: () async{
+                          final res = await Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => work_out(space_name:'유산소' ,)));
+                        },
+                        child:  buildCategory("images/treadmill.jpg", "유산소 존"),
+                      ),InkWell(
+                          onTap: () async{
+                            final res = await Navigator.push(context,
+                                MaterialPageRoute(builder: (context) => work_out(space_name:'중앙 웨이트')));
+                          },
+                        child:  buildCategory("images/bench.jpg", "중앙 웨이트 존"),
+                      ),
+                    ],
+                  ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   InkWell(
-                    onTap: () async{
-                      final res = await Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => work_out(space_name:'유산소' ,)));
-                    },
-                    child:  buildCategory("images/treadmill.jpg", "유산소 존"),
-                  ),InkWell(
-                      onTap: () async{
-                        final res = await Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => work_out(space_name:'중앙 웨이트')));
-                      },
-                    child:  buildCategory("images/bench.jpg", "중앙 웨이트 존"),
-
-                  ),InkWell(
                     onTap: () async{
                       final res = await Navigator.push(context,
                           MaterialPageRoute(builder: (context) => work_out(space_name:'프리웨이트, 머신')));
@@ -63,22 +58,33 @@ class _Reservation extends State<Reservation> {
                   ),
                 ],
               ),
+            ],
+          ),
             ),
-          ],
-        ),
     );
   }
 
     Widget buildCategory(String imageUrl, String title) {
       return Container(
-        width: 160,
+        width: (MediaQuery.of(context).size.width-90)/1.8,
         decoration: BoxDecoration(
-          border: Border.all(width: 1.5),
           borderRadius: BorderRadius.circular(16),
           color:Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius:3,
+              offset: Offset(5,5),
+              color: Colors.grey.withOpacity(0.3),
+            ),
+            BoxShadow(
+              blurRadius:3,
+              offset: Offset(-5,-5),
+              color: Colors.grey.withOpacity(0.3),
+            ),
+          ],
         ),
         child: Padding(
-          padding: const EdgeInsets.all(6.0),
+          padding: const EdgeInsets.all(3.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -87,7 +93,7 @@ class _Reservation extends State<Reservation> {
                 aspectRatio: 1 / 0.85,
               ),
               SizedBox(
-                height: 10,
+                height:MediaQuery.of(context).size.height/50,
               ),
               Text(title),
             ],
