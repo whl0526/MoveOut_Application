262527cb7504adc2b159d683c820424cca6b3b5e
sjms321 <sjms321@naver.com>
2021-09-23 22:51:33 +0900

    Added variables to the api communication model file(model/get_member_info.dart)

--- lib/models/get_member_info.dart
+++ lib/models/get_member_info.dart
@@ -3,16 +3,29 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
+
 class Member_info{
+  late String allcount;
   late String end_date;
   late String enrollment;
+  late String gym;
+  late int key;
+  late String ptdate;
+  late String ptinfo;
+  late String remaining;
   late String start_date;
   late String user_name;
   late String userid;
 
   Member_info({
+    required this.allcount,
     required this.end_date,
     required this.enrollment,
+    required this.gym,
+    required this.key,
+    required this.ptdate,
+    required this.ptinfo,
+    required this.remaining,
     required this.start_date,
     required this.user_name,
     required this.userid,
@@ -32,11 +45,17 @@ Future<Member_info> getMemberInfo(memberId) async{
 
   //받은 데이터정보를 필요한 형태로 저장한다.
   memberInfo = Member_info(
+    allcount: data1["allcount"],
     end_date: data1["end_date"],
     enrollment: data1["enrollment"],
+    gym: data1["gym"],
+    key: data1["key"],
+    ptdate: data1["ptdate"],
+    ptinfo: data1["ptinfo"],
+    remaining: data1["remaining"],
     start_date: data1["start_date"],
     user_name: data1["user_name"],
-    userid:data1["userid"]
+    userid: data1["userid"],
   );
   return memberInfo;
 }
