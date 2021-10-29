5d5a2f92597ac0281af151014af90482dea08463
whl0526 <whl0526@naver.com>
2021-10-29 22:59:33 +0900

    change login_view.dart

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:move_application/models/login.dart';
 import 'package:move_application/style/text_style.dart';
@@ -18,7 +18,6 @@ class LoginIdPw {
 
   LoginIdPw({
     required this.result,
-
     required this.access_token,
   });
 }
@@ -45,99 +44,159 @@ class _loginView extends State<loginView>{
       print(response.body);
     }
     return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
 
-      appBar: AppBar(
-        toolbarHeight: MediaQuery.of(context).size.height*0.095,
-        title: Text(
-          '비켜',
-          style: TitleStyle,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width*0.5,
+                height:MediaQuery.of(context).size.height*0.2,
+                decoration: BoxDecoration(
+
+                    image:
+                    DecorationImage(image:AssetImage('images/logo.png'),
+                    fit:  BoxFit.cover,
+                    ),
+                ),
+                child:Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('Integraged fitness service',
+                    style: TextStyle(
+                        fontSize: 15,
+                      color: Colors.grey,
+                    ),),
+                  ],
+                ),
+
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height*0.05,),
+              Container(
+                width: MediaQuery.of(context).size.width*0.77,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.yellow,
+                    width:1.2,
+                  ),
+                  borderRadius: BorderRadius.circular(35),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 60,
+                      child: Icon(FontAwesomeIcons.user,
+                        size:20,
+                        color:Color(0xFFBB9B9B9),
+                      ),
+                    ),
+                    Expanded(
+                        child:TextField(
+                          onChanged: (A) => ID=A,
+                          decoration: InputDecoration(
+                            border:InputBorder.none,
+                            contentPadding: EdgeInsets.symmetric(vertical: 10),
+                            hintText: '이름을 입력하세요.'
+                          ),
+                        ) )
+                  ],
+                ),
+
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height*0.025,),
+              Container(
+                width: MediaQuery.of(context).size.width*0.77,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.yellow,
+                    width:1.2,
+                  ),
+                  borderRadius: BorderRadius.circular(35),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 60,
+                      child: Icon(
+                        Icons.password_rounded,
+                        size:20,
+                        color: Color(0xFFBB9B9B9),
+                      ),
+                    ),
+                    Expanded(
+                        child:TextField(
+                          obscureText: true,
+                          onChanged: (A) => PW=A,
+                          decoration: InputDecoration(
+                              border:InputBorder.none,
+                              contentPadding: EdgeInsets.symmetric(vertical: 10),
+                              hintText: '등록번호를 입력하세요.'
+                          ),
+                        ) )
+                  ],
+                ),
+
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height*0.03,),
+
+                 FloatingActionButton(
+                  onPressed: () async{
+                    // var data = {
+                    //   "userid" : ID,
+                    //   "password" : PW,
+                    // };
+                    //
+                    // var body = json.encode(data);
+                    //
+                    // http.Response _res = await http.post("http://3.130.113.238:5000/login",
+                    // headers: {"Content-Type": "application/json"},
+                    //   body: body
+                    // );
+                    // print(_res.statusCode);
+                    // print(_res.body);
+                    String apiAddr =
+                        'http://3.130.113.238:5000/login';
+                    var data;
+                    Response response;//http request의 결과 즉 api 호출의 결과를 받기 위한 변수
+                    response = await http.post(apiAddr,
+                        headers: {
+                          'Content-Type': 'application/json; charset=UTF-8',
+                        },
+                        body: jsonEncode({"userid":ID,"password":PW})
+                    );//필요 api 호출
+
+                    data = json.decode(response.body);
+                    LoginIdPw loginIdPw;
+                    loginIdPw = LoginIdPw(
+                        result: data["result"],
+                        access_token: data["access_token"]
+                    );
+
+
+                    if(loginIdPw.result=="OK"){
+                      final res = await Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => HomePage(
+                            access_token: loginIdPw.access_token,
+                            ID: ID,
+                          )));
+                    }else(print("Fail"));
+
+                  },
+                  child:Icon(Icons.login,
+                  size:35,
+                  color: Colors.yellow),
+                  // onPressed: () async{
+                  //   final res = await Navigator.push(context,
+                  //       MaterialPageRoute(builder: (context) => HomePage()));
+                  // }
+                )
+
+
+
+            ],
+          ),
         ),
-
-        centerTitle: false,
-        backgroundColor: Colors.white,
-        shadowColor: null,
-        elevation: 0.0,
-      ),
-
-
-
-      body: Column(
-        children: [
-          SizedBox(height: MediaQuery.of(context).size.height*0.05,),
-          TextField(
-            onChanged: (A) => ID=A,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'ID',
-            ),
-          ),
-          TextField(
-            obscureText: true,
-            onChanged: (A) => PW=A,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'PW',
-            ),
-          ),
-
-             FloatingActionButton(
-
-              onPressed: () async{
-                // var data = {
-                //   "userid" : ID,
-                //   "password" : PW,
-                // };
-                //
-                // var body = json.encode(data);
-                //
-                // http.Response _res = await http.post("http://3.130.113.238:5000/login",
-                // headers: {"Content-Type": "application/json"},
-                //   body: body
-                // );
-                // print(_res.statusCode);
-                // print(_res.body);
-                String apiAddr =
-                    'http://3.130.113.238:5000/login';
-
-                var data;
-
-                Response response;//http request의 결과 즉 api 호출의 결과를 받기 위한 변수
-
-                response = await http.post(apiAddr,
-                    headers: {
-                      'Content-Type': 'application/json; charset=UTF-8',
-                    },
-                    body: jsonEncode({"userid":ID,"password":PW})
-
-                );//필요 api 호출
-
-                data = json.decode(response.body);
-                LoginIdPw loginIdPw;
-                loginIdPw = LoginIdPw(
-                    result: data["result"],
-                    access_token: data["access_token"]
-                );
-
-
-                if(loginIdPw.result=="OK"){
-                  final res = await Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => HomePage(
-                        access_token: loginIdPw.access_token,
-                        ID: ID,
-                      )));
-                }else(print("Fail"));
-
-              },
-              child:Icon(Icons.login),
-              // onPressed: () async{
-              //   final res = await Navigator.push(context,
-              //       MaterialPageRoute(builder: (context) => HomePage()));
-              // }
-            )
-
-
-
-        ],
       ),
     );
   }
