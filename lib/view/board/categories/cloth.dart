2f713101723e1986716acef5000a39f4645c1a06
sjms321 <sjms321@naver.com>
2021-11-08 23:56:36 +0900

    why board has many .dart files?

--- lib/view/board/categories/cloth.dart
+++ lib/view/board/categories/cloth.dart
@@ -13,6 +13,27 @@ class _cloth extends State<cloth>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppbar(widget.space_name),
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              child: ListView.builder(
+                itemBuilder: (BuildContext _context, int i){
+                  return ListTile(
+                    title: Text('게시글이 올라갈 예정입니다.'),
+                  );
+                },
+                itemCount: 5,
+                scrollDirection: Axis.vertical,
+              ),
+              height:MediaQuery.of(context).size.height*0.5,
+              width: MediaQuery.of(context).size.width*0.8,
+            ),
+          )
+          ,
+
+        ],
+      ),
 
     );
   }
